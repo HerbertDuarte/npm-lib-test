81594da43b18aec255ebd2d23cc6d064c29ca929
herberttivic <herbert@tivic.com.br>
2024-07-21 12:29:30 -0300

    feat: paginate props base

--- paginate/index.ts
+++ paginate/index.ts
@@ -1,11 +1,12 @@
 import { PaginateException } from "./exceptions/paginate.exception";
 import { PaginateProps, PaginateResponse, PaginateUtil } from "./paginate.util";
 import { IPaginateUtil } from "./paginate.util.interface";
-
+import { PaginatePropsBase } from "./paginate-props-base.interface";
 export {
   PaginateException,
   PaginateProps,
   PaginateResponse,
   PaginateUtil,
   IPaginateUtil,
+  PaginatePropsBase,
 };
---
+++ paginate/paginate-props-base.interface.ts
@@ -0,0 +1,5 @@
+export interface PaginatePropsBase {
+  pagina: number;
+  itensPorPagina: number;
+  busca?: string;
+}
